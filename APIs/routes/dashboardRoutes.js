5bfd248b9f11e91caabdbe334b9e518ee799df4b
Moetaz <motaz062002@gmail.com>
2025-11-14 12:30:41 +0100

    Remove The old Folder of Projcet

--- APIs/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middleware/verifyToken');
-const { getDashboardData } = require('../controllers/dashboardController');
-
-router.get('/', verifyToken, getDashboardData);
-
-module.exports = router;
